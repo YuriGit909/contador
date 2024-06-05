209d276ab029b539bb2871d5b1fe98f758a426fe
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-06-05 08:40:17 -0300

    Complete (for now)

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,28 @@ button.addEventListener('click', () => {
     var passo = document.getElementById('iPularNum')
     var res = document.getElementById('resultado')
 
-    var num1 = Number(primeiro.value)
-    var num2 = Number(ultimo.value)
-    var pular = Number(passo.value)
-
-    if (num1 == '' && num2 == '' && pular == '') {
-        window.alert('Nenhum campo pode ficar vazio.')
+    if (primeiro.value.lenght == 0 || ultimo.value.lenght == 0 || passo.value.lenght == 0) {
+        res.innerHTML = 'Nenhum campo pode ficar vazio.'
     } else {
-        for(var calc = num1 + pular; calc <= num2; calc++) {
-            res.innerHTML = `${calc}`
+        res.innerHTML = '<p>Contando...</p>'
+        var num1 = Number(primeiro.value)
+        var num2 = Number(ultimo.value)
+        var pular = Number(passo.value)
+
+        if(pular <= 0) {
+            res.innerHTML = `Passo inválido. Considerando PASSO 1.`
+            pular = 1
         }
+   
+        if(num1 < num2) {
+            for(let c = num1; c <= num2; c += pular) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            for(let c = num1; c >= num2; c -= pular) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 })
